44068bdfdf9cc5e1bface9926bce3b80d996c406
Andrii <78295684+syniuk-a@users.noreply.github.com>
2022-08-06 18:50:58 +0300

    update task-03
    1. Напиши скрипт для создания галереи изображений по массиву данных.В HTML есть список ul.gallery.
    
    2. Используй массив объектов images для создания элементов < img > вложенных в < li >.Для создания разметки используй шаблонные строки и метод insertAdjacentHTML().
    
    3. Все элементы галереи должны добавляться в DOM за одну операцию вставки.
    Добавь минимальное оформление галереи флексбоксами или гридами через CSS классы.

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,3 +1,10 @@
+/*
+Напиши скрипт который:
+
+* Посчитает и выведет в консоль количество категорий в ul#categories, то есть элементов li.item.
+* Для каждого элемента li.item в списке ul#categories, найдет и выведет в консоль текст заголовка элемента(тега < h2 >) и количество элементов в категории(всех вложенных в него < li >).
+*/
+
 const itemsEl = document.querySelectorAll('li.item');
 console.log('Кількість категорій -> ', itemsEl.length);
 
@@ -7,3 +14,4 @@ itemsEl.forEach((item) => {
   const itemLength = item.querySelectorAll('.item > ul > li');
   console.log('Елементи -> ', itemLength.length);  
 });
+
--- src/js/task-03.js
+++ src/js/task-03.js
@@ -12,3 +12,28 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+
+/*
+* 1. Напиши скрипт для создания галереи изображений по массиву данных.В HTML есть список ul.gallery.
+
+* 2. Используй массив объектов images для создания элементов < img > вложенных в < li >.Для создания разметки используй шаблонные строки и метод insertAdjacentHTML().
+
+* 3. Все элементы галереи должны добавляться в DOM за одну операцию вставки.
+Добавь минимальное оформление галереи флексбоксами или гридами через CSS классы.
+*/
+
+const galleryListEl = document.querySelector('.gallery');
+galleryListEl.style.display = 'flex';
+galleryListEl.style.justifyContent = 'space-around';
+galleryListEl.style.listStyleType = 'none';
+const galleryItemEl = document.querySelector('li');
+
+const generalGalleryEl = [];
+
+images.forEach((item) => {
+  const itemsEl = `<li><img src="${item.url}" alt="${item.alt}" class="gallary__item" width="400"></li>`;
+  generalGalleryEl.push(itemsEl);
+
+})
+galleryListEl.insertAdjacentHTML('afterbegin', generalGalleryEl);
+console.log(galleryListEl);
